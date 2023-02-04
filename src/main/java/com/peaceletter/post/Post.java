ac60e4aecc0901847fcc7dde6910c4c15815152b
solchan98 <dev.sol0427@gmail.com>
2023-02-04 16:53:59 +0900

    feat: Post 엔티티 간단 작성

---
+++ src/main/java/com/peaceletter/post/Post.java
@@ -0,0 +1,26 @@
+package com.peaceletter.post;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long postId;
+
+    public String title;
+
+    public String content;
+
+    public LocalDateTime createAt;
+
+    public LocalDateTime updateAt;
+
+    public Boolean isDone;
+
+    public String email;
+}
