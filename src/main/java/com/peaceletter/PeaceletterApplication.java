aa7a5220b9e34944591b4f5dcd72db8f18bd2ab9
solchan98 <dev.sol0427@gmail.com>
2023-02-04 16:47:26 +0900

    init: project init

---
+++ src/main/java/com/peaceletter/PeaceletterApplication.java
@@ -0,0 +1,13 @@
+package com.peaceletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaceletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeaceletterApplication.class, args);
+    }
+
+}
