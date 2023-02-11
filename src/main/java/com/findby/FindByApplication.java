6a00d7f5a71809851637ff0d2cb37b312f0a541c
solchan98 <dev.sol0427@gmail.com>
2023-02-11 14:38:56 +0900

    chore: 프로젝트 명 변경 기존 peaceletter에서 findby로 변경

--- src/main/java/com/findby/FindByApplication.java
+++ src/main/java/com/findby/FindByApplication.java
@@ -1,13 +1,13 @@
-package com.peaceletter;
+package com.findby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeaceletterApplication {
+public class FindByApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PeaceletterApplication.class, args);
+        SpringApplication.run(FindByApplication.class, args);
     }
 
 }
--- src/test/java/com/findby/FindByApplicationTests.java
+++ src/test/java/com/findby/FindByApplicationTests.java
@@ -1,10 +1,10 @@
-package com.peaceletter;
+package com.findby;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PeaceletterApplicationTests {
+class FindByApplicationTests {
 
     @Test
     void contextLoads() {
