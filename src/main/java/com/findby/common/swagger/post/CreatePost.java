5b04d362299e74402de53f4c22a42c4ebf1cfc8b
solchan98 <dev.sol0427@gmail.com>
2023-02-13 18:08:56 +0900

    feat: CreatePost, GetPost, UpdatePost 어노테이션 공통 수정 예외에 대한 응답 예시를 설정하는 방법을 찾지 못하여 응답에 대한 예외 메세지를 description으로 알 수 있도록 전체 수정

--- src/main/java/com/findby/common/swagger/post/CreatePost.java
+++ src/main/java/com/findby/common/swagger/post/CreatePost.java
@@ -13,8 +13,8 @@ import java.lang.annotation.Target;
 
 @Operation(summary = "Create Peaceletter", description = "피스레터 생성 API")
 @ApiResponses({
-        @ApiResponse(responseCode = "201", description = "피스레테 생성 성공"),
-        @ApiResponse(responseCode = "400", description = "잘못된 요청인자로 요청하여 실패", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+        @ApiResponse(responseCode = "201", description = "피스레터 생성이 완료되었습니다."),
+        @ApiResponse(responseCode = "400", description = "요청인자를 확인하세요.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
         @ApiResponse(responseCode = "500", description = "서버에서 알 수 없는 에러가 발생하였습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
 })
 @Retention(RetentionPolicy.RUNTIME)
