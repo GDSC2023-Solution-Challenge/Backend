2e7e7e233686b90958a8a1a669fc969eec7c7462
solchan98 <dev.sol0427@gmail.com>
2023-02-13 18:19:12 +0900

    feat: GetPost, UpdatePost 400 응답 메세지 추가 잘못된 요청인자 외 존재하지 않은 경우에 대한 메세지 추가

--- src/main/java/com/findby/common/swagger/post/GetPost.java
+++ src/main/java/com/findby/common/swagger/post/GetPost.java
@@ -15,7 +15,7 @@ import java.lang.annotation.Target;
 @Operation(summary = "Get Peaceletter", description = "단일 피스레터 조회 API")
 @ApiResponses({
         @ApiResponse(responseCode = "200", description = "피스레터 조회를 성공하였습니다."),
-        @ApiResponse(responseCode = "400", description = "요청인자를 확인하세요.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+        @ApiResponse(responseCode = "400", description = "요청인자를 확인하세요.\t\n해당 피스레터가 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
         @ApiResponse(responseCode = "500", description = "서버에서 알 수 없는 에러가 발생하였습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
 })
 @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/com/findby/common/swagger/post/UpdatePost.java
+++ src/main/java/com/findby/common/swagger/post/UpdatePost.java
@@ -12,10 +12,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
 @Operation(summary = "Update Peaceletter", description = "피스레터 업데이트 API")
 @ApiResponses({
         @ApiResponse(responseCode = "200", description = "피스레터 수정이 완료되었습니다."),
-        @ApiResponse(responseCode = "400", description = "요청인자를 확인하세요.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
+        @ApiResponse(responseCode = "400", description = "요청인자를 확인하세요.\t\n해당 피스레터가 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
         @ApiResponse(responseCode = "403", description = "해당 피스레터를 수정할 수 있는 권한이 존재하지 않습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
         @ApiResponse(responseCode = "500", description = "서버에서 알 수 없는 에러가 발생하였습니다.", content = @Content(schema = @Schema(implementation = CommonResponse.class))),
 })
