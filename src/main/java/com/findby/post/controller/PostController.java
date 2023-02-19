a0daa83daff8b1de4d18b756081a82a7f9eb9f9c
solchan98 <dev.sol0427@gmail.com>
2023-02-19 14:28:12 +0900

    feat: PostController 생성 기능 연결 PostController create api PostService create 메서드 연결

--- src/main/java/com/findby/post/controller/PostController.java
+++ src/main/java/com/findby/post/controller/PostController.java
@@ -3,10 +3,12 @@ package com.findby.post.controller;
 import com.findby.common.CommonResponse;
 import com.findby.common.swagger.post.*;
 
+import com.findby.post.service.PostService;
 import com.findby.post.service.dtos.PostRequest;
 import com.findby.post.service.dtos.PostResponse;
 import com.findby.post.service.dtos.PostUpdate;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +18,9 @@ import java.util.Map;
 @Tag(name = "posts", description = "피스레터 API")
 @RestController
 @RequestMapping("api/v1/post")
+@AllArgsConstructor
 public class PostController {
+    private final PostService postService;
 
     @GetPost
     @GetMapping("{postId}")
@@ -32,7 +36,8 @@ public class PostController {
     public ResponseEntity<CommonResponse<PostResponse>> create(
             @RequestBody PostRequest postRequest
     ) {
-        CommonResponse<PostResponse> response = new CommonResponse<>(HttpStatus.CREATED, "피스레터 생성이 완료되었습니다.", PostResponse.API_TEST());
+        PostResponse postResponse = postService.create(postRequest);
+        CommonResponse<PostResponse> response = new CommonResponse<>(HttpStatus.CREATED, "피스레터 생성이 완료되었습니다.", postResponse);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
